b920170a6b4e2c89051b7d262450d3d82b725d9c
Mistly <craftyminer22@gmail.com>
2023-01-25 13:33:27 -0600

    Added PLTL folder

---
+++ PLTL/Assignment 1/A1stephenGarza.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+//Declaring functions
+int square(int num);
+int sumSquare(int a, int b);
+void decreasingSquares(int num);
+
+//Main
+int main(){
+    int num, num2;
+    cout << "Insert a number to square: ";
+    cin >> num;
+    cout << square(num) << endl;
+    cout << "Insert two numbers to square and add together: ";
+    cin >> num >> num2;
+    cout << sumSquare(num, num2) << endl;
+    cout << "Insert a number to square and continuously decrement: ";
+    cin >> num;
+    decreasingSquares(num);
+    return 0;
+}
+
+//Functions
+int square(int num){
+    return num*num;
+}
+int sumSquare(int a, int b){
+    return square(a) + square(b);
+}
+void decreasingSquares(int num){
+    for(int i = num; i>0; i--){
+        string str = i==1?"":", ";
+        cout << square(i) << str;
+    }
+}
