98a808b1550a1994676f1ee143ca3e66d71cc3bb
Mistly <craftyminer22@gmail.com>
2023-01-26 11:44:28 -0600

    getting hex codes to work

--- not 2380/conversion.cpp
+++ not 2380/conversion.cpp
@@ -5,23 +5,26 @@ char convert(int num);
 
 int main(){
     int dec, base, count = 0;
-    string ans = "";
+    int ans[10];
     cout << "Insert a decimal integer: ";
     cin >> dec;
     cout << "What base would you like to convert to?: ";
     cin >> base;
-    while(true){
+    for(int j = 0; j<10; j++){
         int i = dec%base;
         if(dec>0){
-            ans += to_string(i>=10?convert(i):i);
+            ans[j] = i;
             count++;
         }
         else
-            break;
+            j=10;
         dec /= base;
     }
-    for(int i = count; i>=0; i--){
-        cout << ans[i];
+    for(int i = count-1; i>=0; i--){
+        if(ans[i]>=10)
+            cout << convert(ans[i]);
+        else
+            cout << ans[i];
     }
 }
 
