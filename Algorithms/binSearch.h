8cbb69fb0f52dbca0edbac350a9edbd097d0c228
Mistly <craftyminer22@gmail.com>
2023-04-17 10:54:14 -0500

    Binary Search

--- Algorithms/binSearch.h
+++ Algorithms/binSearch.h
@@ -1,2 +1,33 @@
 // Binary search for an integer in an array
-bool binarySearch()
+bool binarySearch(int arr[], int length, int target){
+    int center = length/2;
+    while(length>0){
+        int mid = arr[center];
+        if(mid==target){
+            return true;
+        }
+        else if(mid>target){
+            length /=2;
+            center = length/2;
+        }
+        else{
+            // Increment the array pointer to go 1 index past the center
+            arr += center + 1;
+            // Since we skipped center+1 values, the length should be that much less
+            length -= center + 1;
+            center = length/2;
+        }
+    }
+    return false;
+}
+
+bool binarySearchR(int arr[], int length, int target){
+    if(length==0)
+        return false;
+    else if(arr[length/2+1]==target)
+        return true;
+    else if(arr[length/2+1]>target)
+        return binarySearchR(arr, length/2, target);
+    else
+        return binarySearchR(arr+length/2+1, length/2-1, target);
+}
--- Algorithms/intoAlg.cpp
+++ Algorithms/intoAlg.cpp
@@ -3,26 +3,41 @@
 using namespace std;
 
 #include "seqSearch.h"
+#include "binSearch.h"
 
 int main(){
     srand(time(0));
-    int length = 50;
+    int length = 40;
     int arr[length];
+    int arr2[length];
+    int target{};
 
     for(int i = 0; i<length; i++){
         arr[i] = rand() % 100;
     }
-    int target;
+    cout << "Enter a target: ";
     cin >> target;
 
-    cout << (seqSearch(arr, length, target)?"Yes":"Nope") << '\n';
+    cout << "Seq " << (seqSearch(arr, length, target)?"Success":"Fail") << '\n';
+    cout << "SeqR " << (seqSearchR(arr, length, target)?"Success":"Fail") << '\n';
 
+    cout << "The randomized array:\n";
     for(int num : arr){
         cout << num << " ";
     }
     cout << '\n';
 
-    cout << (seqSearchR(arr, length, target)?"Yes":"Nope") << '\n';
+    arr2[0] = rand() % 10;
+    cout << arr2[0] << ' ';
+    for(int i = 1; i<length; i++){
+        arr2[i] = rand() % 9 + 1;
+        arr2[i] += arr2[i-1];
+        cout << arr2[i] << ' ';
+    }
+    cout << "\nEnter a new target: ";
+    cin >> target;
+    cout << "Binary " <<  (binarySearch(arr2, length, target)?"Success":"Failed") << '\n';
+    cout << "BinaryR " << (binarySearchR(arr2, length, target)?"Success":"Failed") << '\n';
 
     return 0;
 }
